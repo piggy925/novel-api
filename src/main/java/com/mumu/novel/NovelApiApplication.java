2a03307d84eaf4767596ceec5f6091ddfa2cf03c
MuMu <piggy925@163.com>
2022-10-04 23:57:20 +0800

    :tada: 项目初始化

---
+++ src/main/java/com/mumu/novel/NovelApiApplication.java
@@ -0,0 +1,13 @@
+package com.mumu.novel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NovelApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mumu/novel/NovelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumu.novel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
