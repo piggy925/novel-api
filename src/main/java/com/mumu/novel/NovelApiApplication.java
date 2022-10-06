942ef2c31939064ec59d7602e096445253ecb834
MuMu <piggy925@163.com>
2022-10-06 20:00:13 +0800

    :sparkles: 集成MyBatis-Plus

--- src/main/java/com/mumu/novel/NovelApiApplication.java
+++ src/main/java/com/mumu/novel/NovelApiApplication.java
@@ -1,9 +1,11 @@
 package com.mumu.novel;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.mumu.novel.dao.mapper")
 public class NovelApiApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/mumu/novel/core/config/MybatisPlusConfig.java
@@ -0,0 +1,30 @@
+package com.mumu.novel.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+
+/**
+ * MyBatis-Plus 配置类
+ *
+ * @author mumu
+ * @date 2022/10/06
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * MyBatis-Plus 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+
+        return interceptor;
+    }
+
+}
