f7b25b3e223ea4026fe716b7c0586d7a412f1c2b
MuMu <piggy925@163.com>
2022-10-07 13:00:39 +0800

    :sparkles: 集成Caffeine

--- src/main/java/com/mumu/novel/NovelApiApplication.java
+++ src/main/java/com/mumu/novel/NovelApiApplication.java
@@ -1,15 +1,38 @@
 package com.mumu.novel;
 
+import java.util.Map;
+
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableCaching
 @SpringBootApplication
 @MapperScan("com.mumu.novel.dao.mapper")
 public class NovelApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NovelApiApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+            Map<String, CacheManager> beans = context.getBeansOfType(CacheManager.class);
+            log.info("加载了如下缓存管理器：");
+            beans.forEach((k, v) -> {
+                log.info("{}:{}", k, v.getClass().getName());
+                log.info("缓存：{}", v.getCacheNames());
+            });
+        };
     }
 
 }
---
+++ src/main/java/com/mumu/novel/core/config/CacheConfig.java
@@ -0,0 +1,47 @@
+package com.mumu.novel.core.config;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.mumu.novel.core.common.constant.CacheConsts;
+
+/**
+ * 缓存相关配置类
+ *
+ * @author mumu
+ * @date 2022/10/07
+ */
+@Configuration
+public class CacheConfig {
+
+    /**
+     * Caffeine 缓存管理器
+     */
+    @Bean
+    public CacheManager caffeineCacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+
+        List<CaffeineCache> caches = new ArrayList<>(CacheConsts.CacheEnum.values().length);
+        for (CacheConsts.CacheEnum c : CacheConsts.CacheEnum.values()) {
+            if (c.isLocal()) {
+                Caffeine<Object, Object> caffeine = Caffeine.newBuilder().recordStats().maximumSize(c.getMaxSize());
+                if (c.getTtl() > 0) {
+                    caffeine.expireAfterWrite(Duration.ofSeconds(c.getTtl()));
+                }
+                caches.add(new CaffeineCache(c.getName(), caffeine.build()));
+            }
+        }
+
+        cacheManager.setCaches(caches);
+        return cacheManager;
+    }
+
+}
